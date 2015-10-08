e3e9a032923bc37493aa26b858d5ddf75cf9c183
muaad <muadh24@gmail.com>
2015-10-08 14:07:03 +0300

    Added a few more Freshdesk features

--- lib/freshdesk_integration.rb
+++ lib/freshdesk_integration.rb
@@ -40,6 +40,20 @@ class FreshdeskIntegration
 		# `curl -u muaad@sprout.co.ke:freshbok24 -H "Content-Type: application/json" -d '{ "helpdesk_ticket": { "description": "Details about the issue...", "subject": "Support Needed...", "email": "mursal@outerspace.com", "priority": 1, "status": 2 }, "cc_emails": "mursal@freshdesk.com,mohaa@freshdesk.com" }' -X POST http://muaad.freshdesk.com/helpdesk/tickets.json`
 	end
 
+	def find_ticket id
+		get("https://muaad.freshdesk.com/helpdesk/tickets/#{id}.json")
+	end
+
+	def add_comment ticket_id
+		body = {
+		  helpdesk_note: {
+		    body:"Hi tom, Still Angry",
+		    "private" => false
+		  }
+		}
+		post(body, "https://muaad.freshdesk.com/helpdesk/tickets/#{ticket_id}/conversations/note.json")
+	end
+
 	def create_customer name, domains="", description
 		body = {
 			customer: {
@@ -66,7 +80,7 @@ class FreshdeskIntegration
 		}
 		post(body, "https://muaad.freshdesk.com/contacts.json")
 	end
-
+	
 	# def create_user
 	# 	`curl -u muaad@sprout.co.ke:freshbok24 -H "Content-Type: application/json" -X POST -d '{ "user": { "name":"Super Man", "email":"ram@freshdesk.com" }}' http://muaad.freshdesk.com/contacts.json`
 	# end
